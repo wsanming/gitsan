61cafaafc073b18ff707a22be12df61392fb3fbb
sanming <841657622@qq.com>
2020-05-09 15:56:34 +0800

    修改了GitFirst

--- git-first/src/cn/goktech/git/GitFirst.java
+++ git-first/src/cn/goktech/git/GitFirst.java
@@ -4,5 +4,7 @@ public class GitFirst {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("ddd");
+        //test git
+        System.out.println("成功了吗？");
     }
 }
